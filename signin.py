9b72b6416e9c0fb91a28b2212edeb77af8cec02b
Hakan Karagöz <hknkrgzhkn@gmail.com>
2023-04-04 17:47:16 +0300

    Signin, signup ve forgetpass penceleri tamamlandı.
    Tüm arayüz ve veritabanı bağlantıları yapıldı. Veritabanına kullanıcı ekleme ve şifre güncelleme özellikleri eklendi.

--- signin.py
+++ signin.py
@@ -46,12 +46,68 @@ def login_user(event):
 
 
 def forget_pass(event):
+    def change_password(event):
+        if usernameEntry.get() == '' or newpass_entry.get() == '' or confirmpass_entry.get() == '':
+            messagebox.showerror('Error', 'Tüm Alanları Doldurmalısın!', parent=window)
+        elif newpass_entry.get() != confirmpass_entry.get():
+            messagebox.showerror('Error', 'Şifreler Uyuşmuyor!', parent=window)
+        else:
+            con = mysql.connector.connect(host='localhost', user='root', password='', database='userdata')
+            mycursor = con.cursor()
+            query = 'SELECT * FROM data WHERE username=%s'
+            mycursor.execute(query, (user_entry.get(),))
+            row = mycursor.fetchone()
+            if row is None:
+                messagebox.showerror('Error', 'Geçersiz Kullanıcı Adı', parent=window)
+            else:
+                query = 'UPDATE data SET password=%s WHERE username=%s'
+                mycursor.execute(query, (newpass_entry.get(), user_entry.get()))
+                con.commit()
+                con.close()
+                messagebox.showinfo('Success', 'Şifre Değiştirildi. Lütfen yeni şifre ile giriş yapınız')
+                window.destroy()
+
     window = Toplevel()
     window.title('Şifremi Unuttum')
 
     bgPic = ImageTk.PhotoImage(file='background.jpg')
     bglabel = Label(window, image=bgPic)
     bglabel.grid()
+
+    heading_label = Label(window, text='ŞİFRE SIFIRLA', font=('arial', 23, 'bold'), bg='white', fg='magenta2')
+    heading_label.place(x=510, y=60)
+
+    userLabel = Label(window, text='Kullanıcı Adı', font=('arial', 12, 'bold'), bg='white', fg='orchid1')
+    userLabel.place(x=470, y=130)
+
+    user_entry = Entry(window, width=25, fg='magenta2', font=("arial", 11, 'bold'), bd=0, borderwidth=0,
+                       highlightthickness=0)
+    user_entry.place(x=470, y=160)
+
+    Frame(window, width=250, height=2, bg='orchid1').place(x=470, y=180)
+
+    passwordLabel = Label(window, text='Yeni Şifre', font=('arial', 12, 'bold'), bg='white', fg='orchid1')
+    passwordLabel.place(x=470, y=210)
+
+    newpass_entry = Entry(window, width=25, fg='magenta2', font=("arial", 11, 'bold'), bd=0, borderwidth=0,
+                          highlightthickness=0)
+    newpass_entry.place(x=470, y=240)
+
+    Frame(window, width=250, height=2, bg='orchid1').place(x=470, y=260)
+
+    confirmpassLabel = Label(window, text='Yeni Şifre Tekrar', font=('arial', 12, 'bold'), bg='white', fg='orchid1')
+    confirmpassLabel.place(x=470, y=290)
+
+    confirmpass_entry = Entry(window, width=25, fg='magenta2', font=("arial", 11, 'bold'), bd=0, borderwidth=0,
+                              highlightthickness=0)
+    confirmpass_entry.place(x=470, y=320)
+
+    Frame(window, width=250, height=2, bg='orchid1').place(x=470, y=340)
+
+    submitLabel = Label(window, text='                   GÖNDER                   ',
+                        font=('Open Sans', 19, 'bold'), fg='white', bg='magenta2', cursor='hand2')
+    submitLabel.place(x=470, y=390)
+    submitLabel.bind("<Button-1>", change_password)
 
     window.mainloop()
 
